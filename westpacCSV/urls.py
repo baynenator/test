361f6fe1d8e746104a695bbed13eb36638cf186e
Warwick Bayne <Warwick@client-027-011.guest.gnas.otago.ac.nz>
2012-09-02 10:50:21 +1200

    First commit

---
+++ westpacCSV/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+import django.contrib
+django.contrib.admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'westpacCSV.views.home', name='home'),
+    # url(r'^westpacCSV/', include('westpacCSV.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+     url(r'^admin/', include(django.contrib.admin.site.urls)),
+)
